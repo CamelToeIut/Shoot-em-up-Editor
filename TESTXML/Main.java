802e29510cf982fa191e75f79dca39a87ca81014
kollhran <chombart.felix@outlook.com>
2018-02-19 09:19:56 +0100

    Test de la fonction ajout ennemi

--- TESTXML/Main.java
+++ TESTXML/Main.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class Main{
 
 	public static void main(String[] args){
 		BDeditor test = new BDeditor();
-		test.nouveauEnnemi();
-	
+		boolean aoub = true;
+		while(aoub){
+			System.out.println("A ou laisse passer");
+			Scanner sc = new Scanner(System.in);
+			String va = sc.next();
+			if(va=="a"){
+				test.nouveauEnnemi();
+			}else{
+				aoub = false;
+			}
+			
+		}
 		test.EnregistrerFichier();
 	
 	}
